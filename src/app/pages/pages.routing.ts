c1c1bdc221c371b6ee5137195427de8ffbd6179c
johanemst <johanemst@yahoo.com>
2017-01-06 19:57:39 +0100

    Update pages.routing.ts

--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -1,6 +1,6 @@
 import { Routes, RouterModule }  from '@angular/router';
 import { Pages } from './pages.component';
-// noinspection TypeScriptValidateTypes
+
 const routes: Routes = [
   {
     path: 'login',
